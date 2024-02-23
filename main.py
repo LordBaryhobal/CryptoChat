92f23de7ab6405e7119c22e83290747cdb1040ff
LordBaryhobal <lordbaryhobal@gmail.com>
2024-02-23 10:37:02 +0100

    added base files

---
+++ main.py
@@ -0,0 +1,7 @@
+from client.client import Client
+
+if __name__ == "__main__":
+    print("CryptoChat - HES-SO Valais/Wallis - 2024")
+    print("Alexis KUENY & Louis HEREDERO")
+
+    client = Client()
