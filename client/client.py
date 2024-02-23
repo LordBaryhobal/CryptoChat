edcc2d03fad4ff46a2ea5e4ed1b392a97e9cfddb
LordBaryhobal <lordbaryhobal@gmail.com>
2024-02-23 11:42:17 +0100

    added documentation

--- client/client.py
+++ client/client.py
@@ -1,24 +1,49 @@
 import socket
 
-class Client:
-    def __init__(self, host: str, port: int) -> None:
-        self.host = host
-        self.port = port
-        self.socket: socket.socket = None
 
-    def connect(self) -> None:
+class Client:
+    """Simple class to send and receive messages using a TCP socket"""
+
+    def __init__(self, host: str, port: int) -> None:
+        self.host: str = host
+        self.port: int = port
+        self.socket: socket.socket | None = None
+
+    def connect(self) -> bool:
+        """
+        Tries to connect to the server
+
+        Returns: True if the connection was successful, False otherwise
+        """
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
             self.socket.connect((self.host, self.port))
-        except:
-            print("Une erreur est survenue lors de la connexion")
-            return
+        except socket.error:
+            self.socket = None
+            print("An error occurred while trying to connect")
+            return False
+
+        return True
 
     def disconnect(self) -> None:
-        self.socket.close()
+        """
+        Closes the connection with the server
+        """
+
+        if self.socket is not None:
+            self.socket.close()
+            self.socket = None
 
     def send(self, msg: str, msgType: str = "t") -> None:
+        """
+        Sends a message with the given message type
+        Args:
+            msg: the message to send
+            msgType: the message type (one of 't', 'i' or 's')
+        """
+
         payload = b"ISC" + msgType.encode("utf-8")
         length = len(msg)
         payload += length.to_bytes(2, "big")
@@ -30,6 +55,13 @@ class Client:
         self.socket.send(payload)
 
     def receive(self) -> str:
+        """
+        Waits and reads a message from the server
+
+        Returns: the received message, or an empty string
+        if the format is incorrect
+        """
+
         msgBytes = self.socket.recv(4096)
 
         if msgBytes[0:3] != b"ISC":
@@ -47,6 +79,6 @@ class Client:
         msg = b""
         for i in range(0, length):
             offset = 9 + i * 4
-            msg += msgBytes[offset:offset+1]
+            msg += msgBytes[offset:offset + 1]
 
         return msg.decode("utf-8")
