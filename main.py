0026a60638304e916720ed9eff7aa9eba2046d47
LordBaryhobal <lordbaryhobal@gmail.com>
2024-03-01 22:20:51 +0100

    changed main.py to use CLI

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from client.client import Client
+from ui.cli import CLI
 
 if __name__ == "__main__":
     print("CryptoChat - HES-SO Valais/Wallis - 2024")
     print("Alexis KUENY & Louis HEREDERO")
 
     with Client("vlbelintrocrypto.hevs.ch", 6000) as client:
-        msg = input("Enter your message: ")
-        client.send(msg, True)
-        while True:
-            try:
-                msg2 = client.receive()
-                print("Received: " + msg2)
-            except KeyboardInterrupt:
-                break
+        cli = CLI(client)
+        print(client)
+
+        cli.mainloop()
