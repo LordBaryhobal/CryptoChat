7902fb7c38f9b9bf6a64c3be874ca6dd784219dd
alexi <alexis.kueny@outlook.com>
2024-04-19 09:40:30 +0200

    chat message implementation

--- ui/gui.py
+++ ui/gui.py
@@ -2,9 +2,10 @@ import os
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
 
-from client.client import Client
+from client.client import Client, MessageListener
+from client.protocol import Protocol
 from utils import getRootPath
 
 
@@ -17,18 +18,25 @@ class GUI(QApplication):
         super(GUI, self).__init__([])
         self.win: QMainWindow = uic.loadUi(os.path.join(getRootPath(), "res/main.ui"))
         self.win.chatSendBtn.clicked.connect(self.chatMessage)
-
         self.win.show()
         self.client: Client = client
+        self.client.addOnReceiveListener(SentMessageListener(self))
+        self.client.addOnSendListener(SentMessageListener(self))
         self.exec()
 
     def addMessage(self, message: str) -> None:
-        raise NotImplementedError
+        newLabel = QLabel(message)
+        self.win.messagesScroller.layout().addWidget(newLabel)
 
     def chatMessage(self) -> None:
         senderMsg = self.win.chatMsg.text()
         self.client.send(senderMsg)
 
+class SentMessageListener(MessageListener):
+    def __init__(self,gui):
+        self.gui = gui
+    def onMessage(self, msgBytes: bytes) -> None:
+        self.gui.addMessage("> " + Protocol.decode(msgBytes))
 
 if __name__ == '__main__':
     def except_hook(cls, exception, traceback):
