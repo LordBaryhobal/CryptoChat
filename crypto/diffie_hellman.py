43b33ccdd6c79c1e38c69e2c4342d9bb8472eddb
LordBaryhobal <lordbaryhobal@gmail.com>
2024-04-22 18:38:38 +0200

    reformatted diffie_hellman.py + fixed minor issues

--- crypto/diffie_hellman.py
+++ crypto/diffie_hellman.py
@@ -1,7 +1,8 @@
 import random
 
 from crypto.algorithm import Algorithm
-from math_utils.primes import getLargestPrime, getPrimeFactors, isPrime
+from math_utils.primes import getLargestPrime, getPrimeFactors
+from math_utils.ring import Ring
 
 
 class DiffieHellman(Algorithm):
@@ -15,29 +16,33 @@ class DiffieHellman(Algorithm):
 
     def __init__(self):
         super().__init__()
-        while True:
-            self.p = random.randint(2, getLargestPrime(5000))
-            if isPrime(self.p): break
-        print("p = " + str(self.p))
+        self.p = getLargestPrime(random.randint(2, 5000))
+        print(f"p = {self.p}")
+        self.ring = Ring(self.p)
+
         self.prime_factor_list = set(getPrimeFactors(self.p - 1))
-        print(self.prime_factor_list)
+        print(f"prime factors = {self.prime_factor_list}")
+
         self.g = self.find_generator()
-        print(self.g)
-        self.a = random.randint(0,5000)
+        print(f"g = {self.g}")
+
+        self.a = random.randint(2, 5000)
+        print(f"a = {self.a}")
+
         # todo: insert code to fetch secret number B = g^b mod p from server
-        secret = (self.g ** self.a % self.p) * self.B
+        secret = self.ring.fastPow(self.B, self.a)
+        print(f"secret = {secret}")
 
     def find_generator(self):
         for g in range(2, self.p):
-            self.check_valid_generator(g)
             if self.check_valid_generator(g):
                 return g
 
     def check_valid_generator(self, g) -> bool:
         check = False
         for f in self.prime_factor_list:
-            a = (self.p - 1) / f
-            if (g ** a) % 7 == 1 % self.p:
+            a = (self.p - 1) // f
+            if self.ring.fastPow(g, a) == 1:
                 return False
             else:
                 check = True
